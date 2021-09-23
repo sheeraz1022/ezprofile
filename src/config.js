9133ae55764eccc4f2360b0cbffcd2ab3f874655
Sheeraz Shaikh <sheeraz.shaikh@rea-group.com>
2021-09-23 14:28:12 +0800

    Update with Sheeraz's creds

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
     github: {
-        username: 'arifszn', // Your GitHub org/user name. (Required)
+        username: 'sheeraz1022', // Your GitHub org/user name. (Required)
         sortBy: 'stars', // stars | updated
         limit: 8, // How many projects to display.
         exclude: {
@@ -10,81 +10,75 @@ module.exports = {
         }
     },
     social: {
-        linkedin: 'ariful-alam',
-        twitter: 'arif_swozon',
-        facebook: '',
+        linkedin: 'sheerazshaikh',
+        twitter: 'sheerazpetarian',
+        facebook: 'sheeraz1022',
         dribbble: '',
         behance: '',
-        medium: '',
+        medium: 'sheerazpetarian',
         devto: '',
-        website: 'https://arifszn.github.io',
+        website: 'https://sheerazshaikh.com',
         phone: '',
-        email: 'contact@arifszn.com'
+        email: 'sheeraz.shaikh@outlook.com'
     },
     skills: [
-        'PHP',
-        'Laravel',
         'JavaScript',
-        'React.js',
-        'Vue.js',
-        'Node.js',
-        'Jquery',
-        'MySQL',
-        'Git',
+        'React Js',
+        'Node Js',
+        'Next Js',
         'CSS',
-        'Antd',
-        'Tailwind',
-        'Bootstrap',
+        'AWS',
+        'Docker'
     ],
     experiences: [
         { 
-            company: 'Monstarlab Bangladesh',
-            position: 'Software Engineer',
-            from: 'September 2021',
+            company: 'IProperty Sdn Bhd',
+            position: 'Senior Developer',
+            from: 'July 2020',
             to: 'Present'
         },
         { 
-            company: 'Orangetoolz - My Offer 360 Degree',
-            position: 'Jr. Full Stack Engineer',
-            from: 'July 2019',
-            to: 'August 2021'
+            company: 'Miracletek Pvt Ltd',
+            position: 'Senior Software Engineer',
+            from: 'July 2016',
+            to: 'November 2019'
         },
         { 
-            company: 'Techvillage',
-            position: 'Jr. Software Engineer',
-            from: 'January 2019',
-            to: ' June 2019'
+            company: 'Goodcore Software Sdn Bhd',
+            position: 'System Analyst / Senior Developer',
+            from: 'November 2014',
+            to: 'July 2016'
+        },
+        { 
+            company: 'Folio3 Pvt Ltd',
+            position: 'Senior Software Engineer',
+            from: 'January 2013',
+            to: 'November 2014'
+        },
+        { 
+            company: 'Folio3 Pvt Ltd',
+            position: 'Software Engineer',
+            from: 'July 2010',
+            to: 'December 2012'
         }
     ],
     education: [
         { 
-            institution: 'American International University-Bangladesh',
-            degree: 'Bachelor of Science',
-            from: '2015',
-            to: '2019'
-        },
-        { 
-            institution: 'Cantonment College, Jessore',
-            degree: 'Higher Secondary Certificate (HSC)',
-            from: '2012',
-            to: '2014',
-        },
-        { 
-            institution: 'Chowgacha Shahadat Pilot High School',
-            degree: 'Secondary School Certificate (SSC)',
-            from: '2007',
-            to: '2012'
+            institution: 'FAST NUCES Karachi',
+            degree: 'BS Computer Science',
+            from: '2006',
+            to: '2010'
         }
     ],
     blog: {
         // Display blog posts from your medium or dev.to account. (Optional)
-        source: 'dev.to', // medium | dev.to
-        username: 'arifszn',
+        source: 'medium', // medium | dev.to
+        username: 'sheerazpetarian',
         limit: 2 // How many posts to display. Max is 10.
     },
     googleAnalytics: {
         // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-        id: 'G-WLLB5E14M6' // Please remove this and use your own tag id
+        id: 'test' // Please remove this and use your own tag id
     },
     themeConfig: {
         default: 'light',
